f73aa72dfe93dc4262dccb04b74681a6ed158bf9
MichałM <michalhm@gmail.com>
2023-08-26 00:34:54 +0200

    Input file check

--- reader.py
+++ reader.py
@@ -1,15 +1,25 @@
 import sys
 import csv
+import os
 
+def input_file():
+    input_file_name = sys.argv[1]
+    if os.path.exists(input_file_name):
+        return True
+    else:
+        return False
 
 def argv_check():
-    pass
+    if not input_file():
+        print(f"File {sys.argv[1]} not found.")
+    else:
+        pass
 
 def run_app():
     if len(sys.argv) < 4:
         print("FATAL ERROR")
         print("NOT ENOUGH DATA")
     else:
-        pass
+        argv_check()
 
 run_app()
