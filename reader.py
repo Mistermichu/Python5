63e4de36d7d7204e819d51f8976b08b17136dc95
MichałM <michalhm@gmail.com>
2023-08-26 00:24:42 +0200

    Base

---
+++ reader.py
@@ -0,0 +1,15 @@
+import sys
+import csv
+
+
+def argv_check():
+    pass
+
+def run_app():
+    if len(sys.argv) < 4:
+        print("FATAL ERROR")
+        print("NOT ENOUGH DATA")
+    else:
+        pass
+
+run_app()
