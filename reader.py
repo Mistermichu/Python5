aec9f405f2ff541d7d0d56fd35206472f96d1c91
MichałM <michalhm@gmail.com>
2023-08-26 02:12:45 +0200

    Function for argv check

--- reader.py
+++ reader.py
@@ -4,13 +4,19 @@ import os
 
 input_file_data = []
 
+def bad_input_data(value_type, argv_number):
+    print(f"Argv number: {argv_number}. Value provided in argv for {value_type} is incorrect.")
+    print("Try again.")
+
 def read_file():
     with open(sys.argv[1], "r") as input_file:
         for row_data in input_file:
             input_file_data.append(row_data)
 
 def edit_data():
-    
+    number_of_changes = len(sys.argv - 3)
+
+
 
 def input_file():
     input_file_name = sys.argv[1]
@@ -24,9 +30,32 @@ def argv_check():
         print(f"File {sys.argv[1]} not found.")
         return False
     else:
-        print("Nazwa pliku ok")
+        print("Input file detected.")
+        changes = sys.argv[3:]
+        for change_number, requested_change in enumerate(changes):
+            if "," not in requested_change:
+                print("Wrong argv format.")
+                return False
+            column_row_value = requested_change.split(",")
+            if len(column_row_value) != 3:
+                print("Wrong argv format.")
+                return False
+            column = column_row_value[0]
+            row = column_row_value[1]
+            new_value = column_row_value[2]
+            try:
+                int(column)
+            except ValueError:
+                bad_input_data("column", change_number + 3)
+                return False
+            try:
+                int(row)
+            except ValueError:
+                bad_input_data("row", change_number + 3)
+                return False
+            print(f"Requested change: {change_number + 1} in column {column}, row {row}, new value: {new_value}")
         return True
-
+    
 #RUN APP
 if len(sys.argv) < 4:
     print("FATAL ERROR")
@@ -38,3 +67,5 @@ else:
         output_file_name = sys.argv[2]
         read_file()
         print(input_file_data)
+        changes = sys.argv[3:]
+        print(changes)
